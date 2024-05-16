7abaeffb8aa7947f54b5a148fac4eee157365527
bonidandy <bonidandy@gmail.com>
2024-05-16 18:58:03 +0700

    Tugas Machine Learning

---
+++ tugasML/tugas.py
@@ -0,0 +1,33 @@
+#jawaban no 1
+nama = "Bonifasius Dandy Krisnanda"
+nim = "20210801002"
+mata_kuliah = "Machine Learning"
+
+print("DATA DIRI")
+print("Nama : ", nama)
+print("Nim: ", nim)
+print("Perkuliahan: ", mata_kuliah)
+
+#jawaban no 2
+matriksC= [[1,2,3],[4,5,6]]
+print(matriksC)
+
+#jawaban no 3
+matriks = [[2,2],[2,2],[2,2]]
+print(matriks)
+
+#jawaban no 4
+import numpy as np
+
+#jawaban no 5
+from numpy import *
+matriks = range(32)
+matriks = reshape(matriks, (4,8))
+print(matriks)
+
+#jawaban 6
+import numpy as np
+from numpy import *
+
+matriks = np.random.randint(1,5,(3,30))
+print(matriks)
